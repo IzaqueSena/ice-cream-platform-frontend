0d45f7f6de373fb988c21c5488187b60094555a4
Joao <miranda.jp@usp.br>
2023-04-09 16:55:31 -0300

    Botoes 70px + Fornecedores

---
+++ ice-cream-platform/src/app/components/supplier/supplier.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-supplier',
+  templateUrl: './supplier.component.html',
+  styleUrls: ['./supplier.component.css', '../../../assets/styles/nicepage.css']
+})
+export class SupplierComponent {
+
+}
