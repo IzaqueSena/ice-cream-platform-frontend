b67768e24e55f08b39dcb287326270fdae63d4ba
Joao <miranda.jp@usp.br>
2023-04-09 11:12:59 -0300

    TS Encomendas adicionado

--- ice-cream-platform/src/app/components/orders/orders.component.ts
+++ ice-cream-platform/src/app/components/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-orders',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class OrdersComponent {
 
+  constructor(private router: Router) {
+    
+  }
+
+  submitStore(forms: any) {
+    console.log(forms)
+    let store = forms.text
+    console.log(store)
+    // this.router.navigate(['clients-search/clients-listing'],{queryParams: {searchType: 'store', searchValue: store}}).then(() => {window.location.reload();});
+  }
+
+  submitClient(forms: any) {
+    console.log(forms)
+    let client = forms.text
+    console.log(client)
+    // this.router.navigate(['clients-search/clients-listing'],{queryParams: {searchType: 'value', searchValue: value}}).then(() => {window.location.reload();});
+  }
+
+  log(x: any) {
+    console.log(x)
+  } 
 }
