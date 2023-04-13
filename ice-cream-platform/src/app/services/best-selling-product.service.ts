bdf3e9f0c406f2be0ecea398ae95e44ad8d39906
Izaque Sena <izaqueseninha@gmail.com>
2023-04-13 08:31:47 -0300

    mudanças na rotas queries 2 e 3

--- ice-cream-platform/src/app/services/best-selling-product.service.ts
+++ ice-cream-platform/src/app/services/best-selling-product.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from "@angular/core";
   providedIn: 'root'
 })
 export class BestSellingProductService {
-  url = 'http://localhost:8080/produto' 
+  url = 'http://localhost:8080/venda' 
 
   constructor(private http: HttpClient) {
     
